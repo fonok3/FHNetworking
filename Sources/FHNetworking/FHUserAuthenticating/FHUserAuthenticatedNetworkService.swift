2995444f7d26a3b3640ea6d1b98d1c827ae02a46
Florian Herzog <fl.herzog@icloud.com>
2020-01-22 21:17:43 +0100

    #10 ✨ User Authenticated NetworkService

---
+++ Sources/FHNetworking/FHUserAuthenticating/FHUserAuthenticatedNetworkService.swift
@@ -0,0 +1,7 @@
+//
+//  FHNetworking
+//  Copyright © 2019, Florian Herzog
+//
+
+/// NetworkService with is authenticated by user
+public typealias FHUserAuthenticatedNetworkService = FHNetworkService & FHUserAuthenticating
